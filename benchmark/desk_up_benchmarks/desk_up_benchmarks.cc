c60dc80408ca77c89a97151ebc97709188a7a743
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-08-16 19:54:39 +0200

    feature (benchmark): Added coverage for benchmarks
    Used the benchmarks from my repo CMake-test to add them

--- benchmark/desk_up_benchmarks/desk_up_benchmarks.cc
+++ benchmark/desk_up_benchmarks/desk_up_benchmarks.cc
@@ -1,24 +1,14 @@
 #include <benchmark/benchmark.h>
 
-#include "my_class.h"
+#include "desk_up.h"
 
 //Here we include the class we want to benchmark (in this case my_class) as a header file. 
 //We use BENCHMARK() with a function call to measure the speed in which it finishes.
 
 static void BM_ClassCreation(benchmark::State& state) {
   for (auto _ : state){
-    MyClass newClass(2);
+    DeskUpApp app;
   }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_ClassCreation);
-
-
-static void BM_Test(benchmark::State& state) {
-  for (auto _ : state){
-    int a = 3;
-    int b = a%3;
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(BM_Test);
