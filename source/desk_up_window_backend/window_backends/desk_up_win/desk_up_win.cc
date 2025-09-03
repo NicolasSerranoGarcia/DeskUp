575223ee9c358299f387f079703ca50aacf0c2c8
Nico <serranogarcianicolas@gmail.com>
2025-09-03 11:38:17 +0200

    chore (desk_up_win): implemented windows backend for DeskUp

--- source/desk_up_window_backend/window_backends/desk_up_win/desk_up_win.cc
+++ source/desk_up_window_backend/window_backends/desk_up_win/desk_up_win.cc
@@ -1,1 +1,52 @@
 #include "desk_up_win.h"
+
+#include "backend_utils.h"
+
+bool WIN_isAvailable(DU_WindowDevice *){
+    #ifdef _WIN32
+        return true;
+    #endif
+    
+    return false;
+}
+
+DU_WindowDevice * WIN_CreateDevice(void){
+    //set all the functions of a DU_windowDevice variable to the functions of x11. Also set internalData to 
+
+    DU_WindowDevice * device = nullptr;
+
+    device->getWindowHeight = WIN_getWindowHeight;
+    device->isAvailable = WIN_isAvailable;
+
+    windowData * data;
+
+    device->internalData = data;
+    
+    return device;
+}
+
+unsigned int WIN_getWindowHeight(DU_WindowDevice * _this){
+    const windowData * data = (windowData *) _this->internalData;
+    
+    if(!data->hwnd){
+        throw std::invalid_argument("Invalid arguments!");
+    }
+
+    PWINDOWINFO pwi;
+    pwi->cbSize = sizeof(WINDOWINFO);
+    
+    //fills pwi with information about the given window
+    if(!GetWindowInfo(data->hwnd, pwi)){
+        DWORD windowsError = GetLastError();
+        const char * contextMessage = "WIN32 could not return the window height! Cause:";
+
+        const char * errorMessage = getSystemErrorMessageWindows(windowsError, contextMessage);
+
+        throw std::runtime_error(errorMessage);
+        
+    }
+    
+    const unsigned int height = pwi->cyWindowBorders;
+    return height;
+}
+
--- source/desk_up_window_backend/window_backends/desk_up_win/desk_up_win.h
+++ source/desk_up_window_backend/window_backends/desk_up_win/desk_up_win.h
@@ -1,8 +1,27 @@
 #ifndef DESKUPWIN_H
 #define DESKUPWIN_H
 
-typedef struct DU_WindowData{
-    int dummy;
+#include <stdexcept>
+#include <Windows.h>
+
+#include "window_global.h"
+
+// here we define API calls, like WIN_getWindowHeight()
+//we also declare a struct data that carries specific information that the backend needs
+
+typedef struct windowData{
+    HWND hwnd;
 };
+
+DU_WindowBootStrap winWindowDevice = {
+    "win",
+    WIN_CreateDevice
+};
+
+
+DU_WindowDevice * WIN_CreateDevice(void);
+bool WIN_isAvailable(DU_WindowDevice *);
+
+unsigned int WIN_getWindowHeight(DU_WindowDevice * _this);
 
 #endif
