23786d9b2d4d6de9b0e71f6a617cfea9577232f4
Nico <serranogarcianicolas@gmail.com>
2025-09-05 14:16:49 +0200

    refactor (windowDesc.h): the path attribute is now a string instead of std::filesystem::path

--- source/desk_up_window_backend/windowDesc/windowDesc.h
+++ source/desk_up_window_backend/windowDesc/windowDesc.h
@@ -1,12 +1,12 @@
 #ifndef WINDOWDESC_H
 #define WINDOWDESC_H
 
-#include <filesystem>
+#include <string>
 
 // this is an abstract struct that saves all the info needed to restore a window to its original state  
 typedef struct windowDesc{
     int x,y,w,h;
-    std::filesystem::path pathToExec;
+    std::string pathToExec;
 };
 
 #endif
