b38ea19778b049924bd55629952c9e79ad5d9dee
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2025-09-06 19:13:31 +0200

    chore (window_global.h): added remaining pointers to functions to DU_windowDevice

--- source/desk_up_window_backend/window_global.h
+++ source/desk_up_window_backend/window_global.h
@@ -2,22 +2,29 @@
 #define WINDOWGLOBAL_H
 
 #include <iostream>
+#include <vector>
+#include "windowDesc.h"
 
-typedef struct DU_WindowDevice{
+typedef struct DU_windowDevice{
     //here goes all the generic functions that all the backends have
-    unsigned int (*getWindowHeight)(DU_WindowDevice * _this);
-    bool (*isAvailable)(DU_WindowDevice * _this);
+    unsigned int (*getWindowHeight)(DU_windowDevice * _this);
+    unsigned int (*getWindowWidth)(DU_windowDevice * _this);
+    unsigned int (*getWindowXPos)(DU_windowDevice * _this);
+    unsigned int (*getWindowYPos)(DU_windowDevice * _this);
+    
+    std::vector<windowDesc> (*getAllWindows)(void);
 
+    bool (*isAvailable)(DU_windowDevice * _this);
 
     void * internalData;
 };
 
 typedef struct DU_WindowBootStrap{
     const char * name;
-    DU_WindowDevice *(*createDevice)(void);
+    DU_windowDevice *(*createDevice)(void);
 };
 
-DU_WindowDevice * current_window_backend;
+DU_windowDevice * current_window_backend;
 
 extern DU_WindowBootStrap x11WindowDevice;
 extern DU_WindowBootStrap winWindowDevice;
@@ -32,7 +39,7 @@ const unsigned int allWindowBootStrapsLength = 2;
 //function to initialize the backend and choose the correct device
 int DU_Init(){
     for(unsigned int i = 0; i < allWindowBootStrapsLength; i++){
-        DU_WindowDevice * dev = allWindowBootStraps[i].createDevice();
+        DU_windowDevice * dev = allWindowBootStraps[i].createDevice();
         const char * devName = allWindowBootStraps[i].name;
 
         if(!dev || !dev->isAvailable(dev)){
