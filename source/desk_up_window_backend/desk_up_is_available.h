6fa53ec1d252014aaf16c04ea98c3b6be17fd4e7
Nico <serranogarcianicolas@gmail.com>
2025-09-22 23:07:39 +0200

    docs (desk_up_is_available): documented DeskUpIsAvailable struct and file.
    Still trying out doxyfile, so possibly will come back and make changes

--- source/desk_up_window_backend/desk_up_is_available.h
+++ source/desk_up_window_backend/desk_up_is_available.h
@@ -1,8 +1,58 @@
+/**
+ * @file desk_up_is_available.h
+ * @brief A struct wrapper used in DeskUp backend
+ *
+ * This file is part of DeskUp
+ *
+ * @author
+ *   Nicolas Serrano Garcia <serranogarcianicolas@gmail.com>
+ * @date
+ *   2025
+ * @copyright
+ *   Copyright (C) 2025 Nicolas Serrano Garcia
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef DESKUPISAVAILABLE_H
 #define DESKUPISAVAILABLE_H
 
+/**
+ * @struct DeskUpIsAvailable
+ * @brief This struct is a wrapper for a call to a window backend.
+ * @details This call checks if the device it comes from is available on this system. Each backend will have an instance of this struct, generally called <backendName>IsAvailable, that window_core file will use inside
+ * DU_Init()
+ * 
+ * @author Nicolas Serrano Garcia <serranogarcianicolas@gmail.com>
+ * @version 0.1.0
+ * @date 2025
+ */
 struct DeskUpisAvailable{
+    /**
+     * The name of the backend associated with the device call.
+     * @version 0.1.0
+     * @date 2025 
+     */
     const char * name;
+
+    /**
+     * A pointer to function that is used to check whether if a given backend is available in the current device. 
+     * 
+     * @return true if the backend is available in this device, false otherwise
+     * @version 0.1.0
+     * @date 2025
+     */
     bool (*isAvailable)(void);
 };
 
