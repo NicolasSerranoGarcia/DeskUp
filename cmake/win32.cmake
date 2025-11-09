8384a00e3ef494d19dff51c595025f2ba3b8947c
Nico <serranogarcianicolas@gmail.com>
2025-11-09 13:31:54 +0100

    build (CMakeLists.txt): moved qt and win32 dependency to it's own module, and included them in the main CMakeLists.txt. This enforces modularity and cleans up the main CMakeLists from unnecessary noise

---
+++ cmake/win32.cmake
@@ -0,0 +1,28 @@
+# Unlike other cmake modules, this one just defines the win32 interface library
+
+    # --- Create an interface library representing win32. Any sub-library that uses win32 should link against this one --- 
+
+        add_library(desk_up_win32_library INTERFACE)
+
+        target_compile_definitions(desk_up_win32_library INTERFACE
+            WIN32_LEAN_AND_MEAN
+            NOMINMAX
+            _UNICODE
+        )
+
+        target_link_libraries(desk_up_win32_library INTERFACE
+            shlwapi
+            version
+            ole32
+            oleaut32
+            uuid
+            advapi32
+            comdlg32
+            shell32
+            comctl32
+            winmm
+            ws2_32
+            gdi32
+            user32
+            kernel32
+        )
