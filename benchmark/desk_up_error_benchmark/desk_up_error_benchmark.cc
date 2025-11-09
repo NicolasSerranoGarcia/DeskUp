aa15cec363b354352e704bd3413ec012e854df5e
Nico <serranogarcianicolas@gmail.com>
2025-11-09 18:49:53 +0100

    benchmark: the benchmark suite is completely built, and it is ready to be filled with benchmarks

--- benchmark/desk_up_error_benchmark/desk_up_error_benchmark.cc
+++ benchmark/desk_up_error_benchmark/desk_up_error_benchmark.cc
@@ -1,12 +1,12 @@
 #include <benchmark/benchmark.h>
 
-#include "mainWindow.h"
+#include "desk_up_error.h"
 
-static void BM_DeskUpFrontend(benchmark::State& state) {
+static void BM_DeskUpError(benchmark::State& state) {
   for (auto _ : state){
     int i = 0;
     i = state.max_iterations;
   }
 }
 
-BENCHMARK(BM_DeskUpFrontend);
+BENCHMARK(BM_DeskUpError);
