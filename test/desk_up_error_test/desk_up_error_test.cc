0024a53a814d9eb5aea302869b7668c8c8ec6ba5
Nico <serranogarcianicolas@gmail.com>
2025-11-09 19:15:18 +0100

    test (desk_up_error_test): created the first functional test

--- test/desk_up_error_test/desk_up_error_test.cc
+++ test/desk_up_error_test/desk_up_error_test.cc
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "desk_up_error.h"
 
-TEST(DeskUpErrorTest, test){
-    int i = 2;
+using namespace DeskUp;
+
+std::vector<Level> l = {Level::Debug, Level::Default, Level::Error, Level::Fatal, Level::Info, Level::None, Level::Retry, Level::Warning};
+
+TEST(DeskUpErrorTest, checkLevel){
+    for(unsigned int i = 0; i < l.size(); i++){
+        Error err(l[i], ErrType::None, 0, "");
+        ASSERT_EQ(err.level(), l[i]);
+    }
 }
+
